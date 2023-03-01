0310b28cbc9b7cbb6a6b8ac8d54b63b0af4a9326
NPSoftware68 <thanhtra.sqcb@icloud.com>
2023-03-01 10:28:19 +0700

    Initial Commit

---
+++ FoodPin/View/MainView.swift
@@ -0,0 +1,42 @@
+//
+//  MainView.swift
+//  FoodPin
+//
+//  Created by Simon Ng on 23/10/2022.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
+    @State private var selectedTabIndex = 0
+    
+    var body: some View {
+        TabView(selection: $selectedTabIndex) {
+            RestaurantListView()
+                .tabItem {
+                    Label("Favorites", systemImage: "tag.fill")
+                }
+                .tag(0)
+            
+            DiscoverView()
+                .tabItem {
+                    Label("Discover", systemImage: "wand.and.rays")
+                }
+                .tag(1)
+            
+            AboutView()
+                .tabItem {
+                    Label("About", systemImage: "square.stack")
+                }
+                .tag(2)
+        }
+        .accentColor(Color("NavigationBarTitle"))
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
